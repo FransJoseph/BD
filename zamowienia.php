4431fa8cc3dc588823ccce303e1ae6bee258cba1
FransJoseph <139964343+FransJoseph@users.noreply.github.com>
2025-04-14 12:58:38 +0200

    Update zamowienia.php

--- zamowienia.php
+++ zamowienia.php
@@ -1,116 +1,74 @@
-<h4>Zamówienia</h4>
+@font-face {
+    font-family: 'JetBrainsMono';
+    src: url('JetBrainsMono-Light.ttf') format('truetype');
+    font-weight: 300;
+    font-style: normal;
+}
+body{
+    font-family: 'JetBrainsMono', sans-serif;
+    background-color: #000000;
+    color: #eeeeee;
+    background: url('bg1.jpg') no-repeat center center fixed;
+    background-size: cover;
+}   
+#menu{
+    background-color: #4d55cc00;
+    margin-top:20px;
+   
+}
+#main{
+    background-color: #4841cd6e;
+    border:1px solid #B5A8D5;
+    min-height: 400px;
+    margin-top:20px;
+   
 
-<table class="table table-hover table-sm">
-<thead>
-        <tr>
-           
-            <th>Nazwa firmy</th>
-            <th>Nazwa towaru</th>
-            <th>data</th>
-        </tr>
-    </thead>
-    <tbody>
-        <?php
-        include 'dbconfig.php';
-        $conn = new mysqli($server, $user, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Błąd połączenia z BD: " . $conn->connect_error);
-        }
+}
+#footer{
+    background-color: #151159;
+    color:#eeeeee;
+}
 
-        $zapytanie = "SELECT `klienci`.`nazwa` AS 'nazwaKlienta', `towary`.`nazwa` AS 'nazwaTowaru', `operacje`.`data` FROM klienci, operacje, towary WHERE `operacje`.`idk`=`klienci`.`id` AND `operacje`.`idt`=`towary`.`id`";
+#main{
+    margin-left: 10px;
+    margin-right: 10px;
+    padding: 10px;
+}
+.jumbotron {
+    padding: 5px;
+}
+.jumbotron, .jumbotron h1{
+    font-size: 2em;
+    color:#4D55CC;
+    text-shadow: 0px 0px 10px #211C84;
 
-        $result = $conn->query($zapytanie);
+}   
+.table{
+    background-color: #4D55CC;
+    color: #eeeeee;
 
-        if ($result->num_rows > 0) {
-           
-            while ($row = $result->fetch_assoc()) {
-                echo "<tr><td>" . $row["nazwaKlienta"] . "</td><td>" . $row["nazwaTowaru"] . "</td><td>" . $row["data"] . "</td>";
-                echo "<td></tr>\n";
-                
-            }
-        } else {
-            echo "0 results";
-        }
-
-        $conn->close();
-        ?>
-    </tbody>
-</table>
-
-
-
-
-
-
-<form method="POST" action="zapiszOperacje.php" id="form">
-    
-<input type="date" name="dataOperacji"><br>
-<select name="klientID">
-    
-    <?php
-        include 'dbconfig.php';
-        $conn = new mysqli($server, $user, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Błąd połączenia z BD: " . $conn->connect_error);
-        }
-
-        $zapytanie = "SELECT id,nazwa,adres FROM klienci";
-
-        $result = $conn->query($zapytanie);
-
-        if ($result->num_rows > 0) {
-            $licznik = 1;
-            while ($row = $result->fetch_assoc()) {
-              echo "<option value='".$row['id']."'>".$row['nazwa']." [".$row['adres']."]</option>\n"; 
-            }
-        };
-
-
-        $conn->close();
-        ?>
-
-
-
-</select>
-
-<h4>Pozycje:</h4>
-<select name="towar1ID" id="towar">
-    
-    <?php
-        include 'dbconfig.php';
-        $conn = new mysqli($server, $user, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Błąd połączenia z BD: " . $conn->connect_error);
-        }
-
-        $zapytanie = "SELECT id,nazwa FROM towary";
-
-        $result = $conn->query($zapytanie);
-
-        if ($result->num_rows > 0) {
-            $licznik = 1;
-            while ($row = $result->fetch_assoc()) {
-              echo "<option value='".$row['id']."'>".$row['nazwa']."</option>\n"; 
-            }
-        };
-
-        $conn->close();
-        ?>
-
-
-
-</select><br>
-<button id="dodajPozycje">Dodaj pozycję</button>
-<br><br><button type="submit">zatwierdź</button>
-</for>
-
-
-<script>
-    document.getElementById("dodajPozycje").addEventListener("click",addItem);
-        cont towar = document.getElementById("towar");
-    function addItem(){
-        documnet.getElementById("form").appendChild(towar);
-        return false;
-    }
-
-</script>
+}
+#main h2{
+    color: #4D55CC;
+    text-shadow: 0px 0px 10px #211C84;
+    margin:10px;
+    display:block;
+}
+p{
+    display: block;
+    margin:auto;
+}
+form{
+    margin: 10px;
+    padding: 10px;
+    background-color: #4D55CC;
+    color: #eeeeee;
+    border: 1px solid #B5A8D5;
+}
+.del, .edit{
+    color:#ffffff;
+    font-weight: bold;
+}
+select{
+    width: 800px;
+}
